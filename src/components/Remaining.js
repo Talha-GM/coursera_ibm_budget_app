ca5f5deef8d228ea8e19ecf8701b5f81a1694277
Talha Ahmad <tftalha89@gmail.com>
2023-07-14 22:42:16 +0500

    allocation app

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,22 @@
+import { useContext } from "react"
+import { AppContext } from "../context/AppContext"
 
+
+
+const Remaining=()=>{
+    const {expenses , budget} = useContext(AppContext)
+
+    const totalExpenses = expenses.reduce((total,item)=>{
+        return( total = total + item.cost )
+    },0)
+
+    const budgetAlert = totalExpenses > budget ? "alert-danger" : "alert-success"
+
+    return(
+        <div className={`alert ${budgetAlert}`}>
+            <span>Remaining: £{budget - totalExpenses}</span>
+        </div>
+    )
+}
+
+export default Remaining;
